472c8641cd4bf17858214dc0c6e63dfcbe7f790b
mshchts <markoshchutskyy@gmail.com>
2023-11-25 22:44:32 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9d7f8a60-3a77-4bb5-a5fd-ee11d4b6ce05';
+export const MODAL_CLOSE_SEC = 2.5;
